f1c7f666546bdc46f0dcc7388a1f6e04fff48f13
SeungWuk Eun <seungwuk98@snu.ac.kr>
2025-09-23 23:48:59 +0900

    feat: interpreter init

---
+++ include/kecc/ir/Interpreter.h
@@ -0,0 +1,20 @@
+#ifndef KECC_IR_INTERPRETER_H
+#define KECC_IR_INTERPRETER_H
+
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/APSInt.h"
+
+namespace kecc::ir {
+
+class VRegister {
+public:
+};
+
+class Interpreter {
+public:
+private:
+};
+
+} // namespace kecc::ir
+
+#endif // KECC_IR_INTERPRETER_H
